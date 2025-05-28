ff65b5e21cab34155e1610132afecd126f294a4e
yogesh jha <yogeshjha123456@gmail.com>
2025-05-28 20:54:11 +0530

    scope concepts

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -32,3 +32,67 @@ if (true) {
 }
 
 console.log(a); // Logs 20, because var is function scoped or globally scoped
+
+
+//++++++++++++++++++++++++++++++++caccourse++++++++++++++++++++++++++++++++++
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// Function declaration vs Function expression
+
+// when you call a function before its declaration, it works for function declarations but not for function expressions due to hoisting.
+console.log(addone(5))
+
+function addone(num){ // function declaration , this will work because function declaration is hoisted
+    return num + 1
+}
+
+
+// when you call a function before its declaration, it will throw an error for function expressions.
+addTwo(5)
+const addTwo = function(num){ // function expression , this will throw an error because function expression is not hoisted
+    return num + 2
+}
