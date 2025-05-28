f6e95202567e64c8cbe3c557b960bb7b29237bae
yogesh jha <yogeshjha123456@gmail.com>
2025-05-28 20:10:32 +0530

    function concepts

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,3 +1,24 @@
+// concepts of functions in JavaScript
+// Functions in JavaScript are blocks of code designed to perform a particular task.
+// They are executed when they are called or invoked.
+// Functions can take parameters and return values.
+
+// Declaration of functions
+function greet() {
+    console.log("Hello, World!");
+}
+// Function expression
+const greetUser = function(name) {
+    console.log(`Hello, ${name}!`);
+};
+
+// calling the functions
+greet(); // output: Hello, World!
+greetUser("Yogesh"); // output: Hello, Yogesh!
+// Function with parameters and return value
+
+
+// function to add two numbers
 function addTwoNumbers(a, b) {
     return a + b;
 }
@@ -5,36 +26,7 @@ function addTwoNumbers(a, b) {
 let results = addTwoNumbers(5, 10); // output: 15
 console.log(results); // output: 15
 
-
-
-function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
-}
-
-// sayMyName()
-
-// function addTwoNumbers(number1, number2){
-
-//     console.log(number1 + number2);
-// }
-
-function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
-}
-
-const result = addTwoNumbers(3, 5)
-
-// console.log("Result: ", result);
-
-
+// Function for user login message
 function loginUserMessage(username = "sam"){
     if(!username){
         console.log("PLease enter a username");
@@ -47,12 +39,15 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("hitesh"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
+function calculateCartPrice(val1, val2, ...num1){  // rest operator , when you don't know how many arguments will be passed
     return num1
 }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
+
+
+// passing object as an argument to a function
 const user = {
     username: "hitesh",
     prices: 199
@@ -62,12 +57,14 @@ function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-// handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
+        // handleObject(user)
+        handleObject({ //for testing purpose
+            username: "sam",
+            price: 399
+        })
 
+
+// passing array as an argument to a function
 const myNewArray = [200, 400, 100, 600]
 
 function returnSecondValue(getArray){
